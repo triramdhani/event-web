58819931279673d232bf4807a58427e29c287bea
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-12 15:25:21 +0700

    feat: setup material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '@/styles/globals.css'
+import { CacheProvider } from '@emotion/react'
+import type { EmotionCache } from '@emotion/react'
 import type { AppProps } from 'next/app'
+import createEmotionCache from '../../utility/createEmotionCache'
 import { NextPageWithLayout } from './page'
 interface AppPropsLayout extends AppProps {
   Component: NextPageWithLayout
+  emotionCache: EmotionCache
 }
+const clientSideEmotionCache = createEmotionCache()
 
-export default function App({ Component, pageProps }: AppPropsLayout) {
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }: AppPropsLayout) {
   const getLayout = Component.getLayout || ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
