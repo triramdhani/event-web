6a0314268f7e827859f0bd336aa62c2c96e5143b
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-13 12:52:10 +0700

    feat: add tailwindcss and flowbite package

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./node_modules/flowbite-react/**/*.js', './pages/**/*.{ts,tsx}', './public/**/*.html'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('flowbite/plugin')]
+}
